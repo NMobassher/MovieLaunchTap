42bbe48792c7848378096a818e7490e298f76c2d
LoganDMacDonald <logan.macdonald@uoit.net>
2018-12-04 15:15:18 -0500

    made database into context of movies

--- app/src/main/java/uoit/ca/movieapp/MySQLiteHelper.java
+++ app/src/main/java/uoit/ca/movieapp/MySQLiteHelper.java
@@ -1,5 +1,6 @@
 package uoit.ca.movieapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -7,14 +8,19 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class MySQLiteHelper extends SQLiteOpenHelper {
-
+/* values of movie object
+* (String posterPath, boolean adult, String overview, String releaseDate, List<Integer> genreIds, Integer id,
+                 String originalTitle, String originalLanguage, String title, String backdropPath, Double popularity,
+                 Integer voteCount, Boolean video, Double voteAverage)*/
 
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "products.db";
-    public static final String TABLE_NAME = "product";
+    public static final String TABLE_NAME = "moviesToWatch";
     public static final String COL_1 = "id";
-    public static final String COL_2 = "productName";
-    public static final String COL_3 = "price";
+    public static final String COL_2 = "originalTitle";
+    public static final String COL_3 = "popularity";
+    public static final String COL_4 = "voteAverage";
+
 
     public MySQLiteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -27,7 +33,8 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
         String createTable = "CREATE TABLE " + TABLE_NAME + "(" +
                 COL_1 + " Integer PRIMARY KEY AUTOINCREMENT," +
                 COL_2 + " Text NOT NULL," +
-                COL_3 + " number DEFAULT 0)" + ";";
+                COL_3 + " number DEFAULT 0)"+
+                COL_4 + " number DEFAULT 0)"+ ";";
 
         Log.d("DBText", "createTable: " + createTable);
         db.execSQL(createTable);
@@ -39,24 +46,27 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
         this.onCreate(db);
     }
 
-    public void addRecord(Object product) {
-        /*
-        ContentValues values= new ContentValues();
+    public void addRecord(Movie movie) {
+        ContentValues values = new ContentValues();
+        values.put(COL_2,movie.getOriginalTitle());
+        values.put(COL_3,movie.getPopularity());
+        values.put(COL_4,movie.getVoteAverage());
 
-        values.put(COL_2,product.getProductName());
-        values.put(COL_3,product.getPrice());
         SQLiteDatabase db = getWritableDatabase();
         db.insert(TABLE_NAME,null,values);
-
         db.close();
-        */
     }
 
 
-    public void deleteRecord(String nameInput) {
-
+    public void deleteRecord(String movieName) {
         SQLiteDatabase db = getWritableDatabase();
-        db.execSQL("Delete from " + TABLE_NAME + " where " + COL_2 + "='" + nameInput + "';");
+        db.execSQL("Delete from " + TABLE_NAME + " where " + COL_2 + "='" + movieName + "';");
+        db.close();
+    }
+
+    public void deleteAll() {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("Delete from " + TABLE_NAME + ";");
         db.close();
     }
 
